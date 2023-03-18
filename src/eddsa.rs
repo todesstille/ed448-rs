ef3142a10aa25ac01e841a8a5b139e7378d416cc
Dmitry Redkin <tanatonaut@gmail.com>
2023-03-18 20:15:23 +0500

    backup

--- src/eddsa.rs
+++ src/eddsa.rs
@@ -1,3 +1,5 @@
+use sha3::{Shake256, digest::{Update, ExtendableOutput, XofReader}};
+
 pub fn clamp(pk: &mut [u8]) {
     
     pk[0] &= 0xfc;
@@ -6,4 +8,13 @@ pub fn clamp(pk: &mut [u8]) {
 
 }
 
-pub fn hash_with_dom() {}
+pub fn sha3_57 (input: &[u8], mut output: &[u8]) {
+    let hasher = Shake256::default();
+    hasher.update(input);
+    let mut reader = hasher.finalize_xof();
+    reader.read(output);
+}
+
+pub fn hash_with_dom() {
+
+}
--- src/goldilocks.rs
+++ src/goldilocks.rs
@@ -1,8 +1,6 @@
 use std::result;
 
-use sha3::{Shake256, digest::{Update, ExtendableOutput, XofReader}};
-
-use crate::{scalar::{decode_long, halve}, extended_point::precomputed_scalar_mul, eddsa::clamp};
+use crate::{scalar::{decode_long, halve}, extended_point::precomputed_scalar_mul, eddsa::{clamp, sha3_57}};
 
 pub type PrivateKey = [u8; 57];
 pub type PublicKey = [u8; 57];
@@ -13,17 +11,9 @@ pub fn hex_to_private_key(hexx: &str) -> PrivateKey {
     p
 }
 
-pub fn sha3_57 (pk: &PrivateKey) -> [u8; 57] {
-    let mut hasher = Shake256::default();
-    hasher.update(pk);
-    let mut reader = hasher.finalize_xof();
-    let mut result = [0u8; 57];
-    reader.read(&mut result);
-    result
-}
-
 pub fn private_to_secret(pk: &PrivateKey) -> PrivateKey {
-    let mut sk = sha3_57(&pk);
+    let mut sk: PrivateKey = [0; 57];
+    sha3_57(pk, &sk);
     sk[56] |= 0x80;
     sk
 }
@@ -50,14 +40,6 @@ mod tests {
     pub fn test_decode_hex () {
         let pk = hex_to_private_key("a8ea212cc24ae0fd029a97b64be540885af0e1b7dc9faf4a591742850c4377f857ae9a8f87df1de98e397a5867dd6f20211ef3f234ae71bc56");
         let exp: PrivateKey = [168, 234, 33, 44, 194, 74, 224, 253, 2, 154, 151, 182, 75, 229, 64, 136, 90, 240, 225, 183, 220, 159, 175, 74, 89, 23, 66, 133, 12, 67, 119, 248, 87, 174, 154, 143, 135, 223, 29, 233, 142, 57, 122, 88, 103, 221, 111, 32, 33, 30, 243, 242, 52, 174, 113, 188, 86];
-        assert_eq!(pk, exp);
-    }
-
-    #[test]
-    pub fn test_sha3_57() {
-        let mut pk = hex_to_private_key("a8ea212cc24ae0fd029a97b64be540885af0e1b7dc9faf4a591742850c4377f857ae9a8f87df1de98e397a5867dd6f20211ef3f234ae71bc56");
-        pk = sha3_57(&pk);
-        let exp: [u8; 57] = [20, 19, 130, 30, 214, 112, 131, 200, 85, 198, 219, 68, 5, 221, 79, 165, 253, 236, 57, 225, 199, 97, 190, 20, 21, 98, 60, 28, 32, 44, 92, 181, 23, 110, 87, 136, 48, 55, 43, 126, 7, 235, 30, 249, 207, 113, 177, 149, 24, 129, 92, 77, 160, 253, 45, 53, 148];
         assert_eq!(pk, exp);
     }
 
