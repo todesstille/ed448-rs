bf03608aafce7c745914eebaae928d8c4c70e159
Dmitry Redkin <tanatonaut@gmail.com>
2023-08-27 22:21:16 +0300

    errors

--- src/lib.rs
+++ src/lib.rs
@@ -10,18 +10,3 @@ mod decaf_wnaf_table;
 mod goldilocks;
 mod eddsa;
 mod errors;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
