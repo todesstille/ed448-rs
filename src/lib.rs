23bf1abb8aea0e2ca6832e47c0a10bba4b5a1495
Dmitry Redkin <tanatonaut@gmail.com>
2023-08-30 23:20:31 +0300

    moved to submodules

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,6 @@ mod karatsuba_square_32;
 mod extended_point;
 mod decaf_combs_32;
 mod decaf_wnaf_table;
-mod goldilocks;
+pub mod goldilocks;
 mod eddsa;
-mod errors;
-
-pub use goldilocks::ed448_verify;
-pub use errors::LibgoldilockErrors;
+pub mod errors;
