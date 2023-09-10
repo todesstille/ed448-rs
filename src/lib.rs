357b48f86dbc352969b3d748c32eaecb938c71f0
Dmitry Redkin <tanatonaut@gmail.com>
2023-09-10 18:50:54 +0300

    signing key

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub mod goldilocks;
 mod eddsa;
 pub mod errors;
 
-use goldilocks::hex_to_private_key;
+use goldilocks::{hex_to_private_key, ed448_derive_public};
 use crate::errors::LibgoldilockErrors;
 
 pub trait PrehashSigner<S> {
@@ -27,10 +27,30 @@ pub struct VerifyingKey {
     key: [u8; 57],
 }
 
+pub struct SigningKey {
+    secret_key: SecretKey,
+    verifying_key: VerifyingKey,
+}
+
 impl SecretKey {
     pub fn from_str(str: &str) -> Result<Self, LibgoldilockErrors> {
     let key = hex_to_private_key(str); 
         Ok(Self {key})
+    }
+}
+
+impl SigningKey {
+    pub fn from_str(str: &str) -> Result<Self, LibgoldilockErrors> {
+        let private_key = hex_to_private_key(str);
+        let public_key = ed448_derive_public(&private_key);
+        let secret_key = SecretKey {key: private_key};
+        let verifying_key = VerifyingKey {key: public_key};
+        
+        Ok(Self {secret_key, verifying_key})
+    }
+
+    pub fn verifying_key(&self) -> &VerifyingKey {
+        &self.verifying_key
     }
 }
 
